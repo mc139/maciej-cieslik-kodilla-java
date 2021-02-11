bead0f8f0dd7690d519d2973d223823b5214493a
mc139 <maciejandrzejcieslik@gmail.com>
2021-02-11 18:30:57 +0000

    6.3 fixed

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -4,28 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OddNumbersExterminator {
-    @Override
-    public String toString() {
-        return "OddNumbersExterminator{" +
-                "numbers=" + numbers +
-                '}';
-    }
-
-    public List<Integer> numbers = new ArrayList<>();
-
-
-    public OddNumbersExterminator(List<Integer> numbers) {
-        this.numbers = numbers;
-    }
 
     public List<Integer> exterminate(List<Integer> numbers) {
+        List<Integer> evenNumbers = new ArrayList<>();
         for (int i = 0; i < numbers.size(); i++)
-            if (numbers.get(i) % 2 != 0 || numbers.contains(0)) {
-                numbers.remove(i);
+            if (numbers.get(i) % 2 == 0) {
+                evenNumbers.add(numbers.get(i));
             }
-        return numbers;
+        return evenNumbers;
     }
-
-
 }
-
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,8 +1,6 @@
 package com.kodilla.testing.collection;
 
 import org.junit.jupiter.api.*;
-
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,55 +29,31 @@ public class CollectionTestSuite {
     @DisplayName("When created List is Empty checking if Class works correctly")
     @Test
     void testOddNumberExterminatorEmptyList() {
-
-
         //GIVEN
         List<Integer> numbers = new ArrayList<>();
-        OddNumbersExterminator test1 = new OddNumbersExterminator(numbers);
-
+        List<Integer> expected = new ArrayList<>();
+        OddNumbersExterminator underTest = new OddNumbersExterminator();
 
         //WHEN
+        List<Integer> result = underTest.exterminate(numbers);
 
-        for (Integer testInteger : test1.numbers){
-            if(testInteger == null){
-                System.out.println("List is Empty!");
-            }
-        }
-
-
-            //THEN
-            Assertions.assertTrue(test1.exterminate(numbers).isEmpty());
-
+        //THEN
+        Assertions.assertEquals(expected, result);
     }
 
     @DisplayName("When created List with numbers range 0-100 checking is the Class return correct values")
     @Test
-    void  testOddNumbersExterminatorNormalList(){
+    void testOddNumbersExterminatorNormalList() {
 
         //GIVEN
-        List<Integer> numbers = new ArrayList<>();
-        OddNumbersExterminator test2 = new OddNumbersExterminator(numbers);
-
-        for (int i=0; i <100; i++){
-            test2.numbers.add(i);
-        }
-        for (Integer i=0; i <100; i++){
-            test2.exterminate(numbers);
-        }
+        List<Integer> numbers = List.of(1, 2, 35, 6, 4, 3, 2, 32, 12, 5, 6);
+        List<Integer> expected = List.of(2, 6, 4, 2, 32, 12, 6);
+        OddNumbersExterminator underTest = new OddNumbersExterminator();
 
         //WHEN
-        Integer firstExpectedNumber = 2;
-        Integer secondExpectedNumber = 4;
-        Integer thirdExpectedNumber = 8;
-
-        Integer firstNumberResult = test2.numbers.get(0);
-        Integer secondNumberResult = test2.numbers.get(1);
-        Integer thirdNumberResult = test2.numbers.get(3);
+        List<Integer> result = underTest.exterminate(numbers);
 
         //THEN
-        Assertions.assertEquals(firstExpectedNumber,firstNumberResult);
-        Assertions.assertEquals(secondExpectedNumber,secondNumberResult);
-        Assertions.assertEquals(thirdExpectedNumber,thirdNumberResult);
-
+        Assertions.assertEquals(expected, result);
     }
 }
