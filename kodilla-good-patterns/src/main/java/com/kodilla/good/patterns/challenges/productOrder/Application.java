cf5bd6117d6a070f6e70097901d77ef8982fc164
mc139 <maciejandrzejcieslik@gmail.com>
2021-04-03 16:44:51 +0100

    ?

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/productOrder/Application.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/productOrder/Application.java
@@ -1,13 +1,17 @@
 package com.kodilla.good.patterns.challenges.productOrder;
 
+import java.time.LocalDateTime;
+
 public class Application {
 
     public static void main(String[] args) {
 
+        ShopUser user = new ShopUser("john","Smith",2000);
+
         PurchaseRequestRetriever purchaseRequestRetriever = new PurchaseRequestRetriever();
         PurchaseRequest purchaseRequest = new PurchaseRequestRetriever().retrieve();
 
-        ProductOrderService productOrderService = new ProductOrderService()
+//        ProductOrderService productOrderService = new ProductOrderService().process(user, LocalDateTime.now();
 
     }
 }
